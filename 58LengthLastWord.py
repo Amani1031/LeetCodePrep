1b4be0d60d7801bafe597c05aa9e7324008f9b21
Amani1031 <aarora09@calpoly.edu>
2022-09-08 13:06:55 +0800

    58

--- 58LengthLastWord.py
+++ 58LengthLastWord.py
@@ -1,12 +1,24 @@
 
+#Attempt 1 : 61ms
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         x = str.split(' ')
         print(x)
         return len(x[-1])
 
+#Attempt 2 : 62 ms
+class Solution2:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] != ' ':
+                length += 1
+            else:
+                if length > 0: return length
+        return length
+
 '''
-class Solution {
+
     public int lengthOfLastWord(String s) {
         int length = 0;
 		
